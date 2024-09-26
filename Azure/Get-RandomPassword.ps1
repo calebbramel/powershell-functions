ef4155e81b897e893163016d2b03dd8da1aea801
Caleb <65420922+calebbramel@users.noreply.github.com>
2024-09-26 11:44:29 -0400

    Create Get-RandomPassword.ps1

---
+++ Azure/Get-RandomPassword.ps1
@@ -0,0 +1,24 @@
+function Get-RandomPassword {
+    $PasswordLength = 3
+
+    $SourceWords = 'Adams', 'Alpha', 'Boston', 'Bravo', 'Charlie', 'Chicago', 'Delta', 'Denver', 'Easy',
+                   'Echo', 'Foxtrot', 'Frank', 'George', 'Golf', 'Henry', 'Hotel', 'Ida', 'India', 'John', 
+                   'Juliet', 'Kilo', 'King', 'Lima', 'Lincoln', 'Mary', 'Mike', 'November', 'Ocean', 'Oscar', 
+                   'Papa', 'Peter', 'Quebec', 'Queen', 'Roger', 'Romeo', 'Sierra', 'Sugar', 'Tango', 'Thomas', 
+                   'Uniform', 'Union', 'Victor', 'Whiskey', 'William', 'Yankee', 'Young', 'Zulu'
+    $SourceNumbers = 0..9
+
+    $PasswordWords = @()
+
+    while ($PasswordWords.Count -lt $PasswordLength) {
+        $RandomWord = Get-Random -InputObject $SourceWords
+        if ($RandomWord -notin $PasswordWords) {
+            $PasswordWords += $RandomWord
+        }
+    }
+
+    $RandomNumber = Get-Random -InputObject $SourceNumbers
+
+    $Password = ($PasswordWords -join '-') + "$RandomNumber"
+    return $Password
+}
